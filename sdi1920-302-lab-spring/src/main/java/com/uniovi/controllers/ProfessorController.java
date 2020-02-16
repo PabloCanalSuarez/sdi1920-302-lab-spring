a5aa42d21899696cf66735b063fcc7243328a088
Pablo Cañal Suárez <uo264903@uniovi.es>
2020-02-16 01:04:22 +0100

    SDI–session2-Ejercicio-complementario4

--- sdi1920-302-lab-spring/src/main/java/com/uniovi/controllers/ProfessorController.java
+++ sdi1920-302-lab-spring/src/main/java/com/uniovi/controllers/ProfessorController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.uniovi.entities.Mark;
 import com.uniovi.entities.Professor;
 import com.uniovi.services.ProfessorService;
 
@@ -47,16 +46,26 @@ public class ProfessorController {
 		return "redirect:/professor/list";
 	}
 	
-	@RequestMapping(value="/professor/edit/{id}", method=RequestMethod.POST)
-	public String setEdit(Model model, @PathVariable Long id, @ModelAttribute Professor prof) {
-		prof.setId(id);
-		professorService.addProfessor(prof);
-		return "redirect:/professor/details/" + id;
-	}
-	
-	@RequestMapping(value = "/professor/edit/{id}")
-	public String getEdit(Model model, @PathVariable Long id) {
-		model.addAttribute("professor", professorService.getProfessor(id));
+
+	/**
+	 * Muestra los datos del profesor a editar.
+	 */
+	@RequestMapping(value = "/professor/edit", method = RequestMethod.POST)
+	public String getEdit(Model model, @ModelAttribute Professor professor) {
+		model.addAttribute("professor", professorService.getProfessor(professor.getId()));
+		System.out.println(professor.toString());
+		System.out.println(professorService.getProfessor(professor.getId()).toString());
 		return "professor/edit";
+	}
+
+	/**
+	 * Introduce los datos editados en la BD y cambia a /professor/details.
+	 */
+	@RequestMapping(value = "/professor/edit")
+	public String setEdit(Model model, @ModelAttribute Professor professor) {
+		model.addAttribute("professor", professorService.getProfessor(professor.getId()));
+		professor.setId(professor.getId());
+		professorService.addProfessor(professor);
+		return "redirect:/professor/details/" + professor.getId();
 	}
 }
